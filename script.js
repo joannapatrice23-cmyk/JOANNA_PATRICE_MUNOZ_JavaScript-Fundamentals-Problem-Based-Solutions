8bf9e8d5c18ea1ea7a919a5095d1506ce3483e26
joannapatrice23-cmyk <joannapatrice23@gmail.com>
2026-04-25 12:58:33 +0000

    adding function button in js

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ let goal = Number(localStorage.getItem("goal")) || 2000;
 let intake = Number(localStorage.getItem("intake")) || 0;
 
 updateUI();
+
+function setGoal() {
+  const input = document.getElementById("goalInput").value;
+  if (!input || input <= 0) return;
+
+  goal = Number(input) * 1000; // convert L → ml
+  intake = 0;
+
+  localStorage.setItem("goal", goal);
+  localStorage.setItem("intake", intake);
+
+  updateUI();
+}
